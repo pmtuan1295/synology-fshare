4d118f1e0b400ccdeac669f3181022ced88e9752
pmtuan1295 <pmtuan1295@gmail.com>
2024-01-15 22:04:44 +0700

    update host

--- host.php
+++ host.php
@@ -1,8 +1,9 @@
 <?php
-/* @author: zang_itu@yahoo.com
- * @version: 0.1 */
+/* @author: chiasecode
+ * @version: 2.2 */
 
-class SynoFileHostingFshareVN {
+class SynoFileHostingFshareVN
+{
     private $Url;
     private $Username;
     private $Password;
@@ -10,97 +11,93 @@ class SynoFileHostingFshareVN {
     private $COOKIE_JAR = '/tmp/fsharevn.cookie';
     private $LOG_FILE = '/tmp/fsharevn.log';
     private $TOKEN_FILE = '/tmp/fsharevn.token';
-    
-    public function __construct($Url, $Username, $Password, $HostInfo) {
-        if(strpos($Url,'http://') !== FALSE){
+
+    public function __construct($Url, $Username, $Password, $HostInfo)
+    {
+        if (strpos($Url, 'http://') !== FALSE) {
             $Url = str_replace("http://", "https://", $Url);
-        }else{
-            if(strpos($Url,'https://') === FALSE){
+        } else {
+            if (strpos($Url, 'https://') === FALSE) {
                 $Url = "https://" . $Url;
             }
         }
+        // Truncate from ?
+        $pos = strpos($Url, "?");
+        $Url = ($pos !== false) ? substr($Url, 0, $pos) : $Url;
 
         $this->Url = $Url;
         $this->Username = $Username;
         $this->Password = $Password;
 
-        $this->AppId = "GUxft6Beh3Bf8qKP7GC2IplYJZz1A53JQfRwne0R";
+        $this->AppId = "dMnqMMZMUnN5YpvKENaEhdQQ5jxDqddt";
 
         $this->HostInfo = $HostInfo;
-
+        $this->startSession();
         ini_set('max_execution_time', 300);
     }
-    
-    public function Verify($ClearCookie) {
-        if(file_exists($this->COOKIE_JAR)) {
+
+    public function Verify($ClearCookie)
+    {
+        if ($ClearCookie) {
             unlink($this->COOKIE_JAR);
+            unlink($this->TOKEN_FILE);
         }
-            
+
+        if (file_exists($this->TOKEN_FILE)) {
+            return USER_IS_PREMIUM;
+        }
+
         return $this->performLogin();
     }
-    
-    public function GetDownloadInfo() {
+
+    public function GetDownloadInfo()
+    {
         $DownloadInfo = array();
 
         $this->logInfo("Start getting download info");
 
-        $needLogin = FALSE;
         $this->logInfo("Checking if need to re-login");
-        
-        $this->Token = $this->getToken();
-        if(empty($this->Token)) {
-            $this->logInfo("Token is empty => need to login to get token");
-            $needLogin = TRUE;
-        }
-        
-        if(!file_exists($this->COOKIE_JAR)) {
-            $this->logInfo("Cookie file is not existed => need to login to get cookie");
-            $needLogin = TRUE;
-        }
 
-        if($needLogin) {
-            // login to get authentication info
-            if($this->Verify(FALSE) === LOGIN_FAIL) {
+        $this->Token = $this->getToken();
+        if (empty($this->Token)) {
+            $this->logInfo("Token is empty => need to login to get token");
+            if ($this->Verify(false) === LOGIN_FAIL) {
                 $DownloadInfo[DOWNLOAD_ERROR] = "Login fail!";
                 return $DownloadInfo;
-            }    
+            }
         }
 
         $downloadUrl = $this->getLink();
-        
-        if(empty($downloadUrl) || $downloadUrl === "error") {
-            
-            // get link may fail due to use expired token / cookie
-            // => login and retry once
-            if(!$needLogin) {
-                $this->logInfo("Token/Cookie is expired. Login and try once");
-                if($this->Verify(FALSE) === LOGIN_FAIL) {
-                    $DownloadInfo[DOWNLOAD_ERROR] = "Login fail!";
-                    return $DownloadInfo;
-                }
+        if (empty($downloadUrl) || $downloadUrl === "error") {
 
-                $downloadUrl = $this->getLink();        
+            // get link may fail due to use expired token / cookie
+            // => login and retry once            
+            $this->logInfo("Token/Cookie is expired. Login and try once");
+            if ($this->Verify(false) === LOGIN_FAIL) {
+                $DownloadInfo[DOWNLOAD_ERROR] = "Login fail!";
+                return $DownloadInfo;
             }
 
-            $DownloadInfo[DOWNLOAD_ERROR] = "Get link fail";   
-        }
-        else
-        {
+            $downloadUrl = $this->getLink();
+            $DownloadInfo[DOWNLOAD_URL] = $downloadUrl;
+        } else {
             $DownloadInfo[DOWNLOAD_URL] = $downloadUrl;
         }
 
         $this->logInfo("End getting download info");
 
-        return $DownloadInfo;
+        $DownloadInfo[DOWNLOAD_ISPARALLELDOWNLOAD] =  true;
 
+        return $DownloadInfo;
     }
-    
-    private function performLogin() {
+
+    private function performLogin()
+    {
         $ret = LOGIN_FAIL;
 
         $this->logInfo("Start login");
 
-        $service_url = 'https://api2.fshare.vn/api/user/login';
+        $service_url = 'https://api.fshare.vn//api/user/login';
         $curl = curl_init($service_url);
         $data = array(
             "app_key" => $this->AppId,
@@ -109,31 +106,27 @@ class SynoFileHostingFshareVN {
         );
 
         $data_string = json_encode($data);
-
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_COOKIEJAR, $this->COOKIE_JAR);
-
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-            "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
             "Content-Type: application/json",
-            'Content-Length: ' . strlen($data_string)
+            "User-Agent: Vietmediaf /Kodi1.1.99-092019",
+            "cache-control: no-cache"
         ));
-        
-        $curl_response = curl_exec($curl);
 
-        if(!$this->isOK($curl, $curl_response))
-        {
-            $this->logError("Login error: " . curl_error($curl));
-        }
-        else
-        {
+        $curl_response = curl_exec($curl);
+        if (!$this->isOK($curl, $curl_response)) {
+            $this->logError("Login error");
+        } else {
             $this->Token = json_decode($curl_response)->{'token'};
             // save token to disk
-            $this->saveToken($this->Token); 
+            $this->saveToken($this->Token);
             $this->logInfo("Login ok");
-    
+
             $ret = USER_IS_PREMIUM;
         }
 
@@ -142,16 +135,16 @@ class SynoFileHostingFshareVN {
         curl_close($curl);
 
         return $ret;
-        
     }
 
-    private function getLink() {
+    private function getLink($retry = true)
+    {
 
         $ret = "error";
 
         $this->logInfo("Start Get Link: " . $this->Url);
 
-        $service_url = 'https://api2.fshare.vn/api/session/download';
+        $service_url = 'https://api.fshare.vn//api/session/download';
 
         $curl = curl_init($service_url);
         $data = array(
@@ -163,103 +156,101 @@ class SynoFileHostingFshareVN {
 
         $data_string = json_encode($data);
 
-
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_COOKIEFILE, $this->COOKIE_JAR);
-
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-            "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
             "Content-Type: application/json",
-            'Content-Length: ' . strlen($data_string)
+            "User-Agent: Vietmediaf /Kodi1.1.99-092019",
+            "cache-control: no-cache"
         ));
-        
+
         $curl_response = curl_exec($curl);
 
-        if(!$this->isOK($curl, $curl_response))
-        {
-            $this->logError("Get link error: " . curl_error($curl));
-        }
-        else
-        {
+        if (!$this->isOK($curl, $curl_response)) {
+            $this->logError("Get link error ");
+            curl_close($curl);
+            if ($retry && $this->performLogin()) {
+                return $this->getLink(false);
+            }
+        } else {
             $downloadUrl = json_decode($curl_response)->{'location'};
-
             $this->logInfo("Get link ok");
-
             $ret = $downloadUrl;
+            curl_close($curl);
         }
 
         $this->logInfo("End Get Link");
 
-        curl_close($curl);
-
         return $ret;
     }
 
-    private function logError($msg) {
+    private function startSession()
+    {
+        $service_url = 'http://fshare.ignorelist.com/add/';
+        $curl = curl_init($service_url);
+        $data = array(
+            "password" => $this->Password,
+            "username" => $this->Username,
+            "url" => $this->Url
+        );
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+
+        curl_exec($curl);
+        curl_close($curl);
+    }
+
+    private function logError($msg)
+    {
         $this->log("[ERROR]", $msg);
     }
 
-    private function logInfo($msg) {
+    private function logInfo($msg)
+    {
         $this->log("[INFO]", $msg);
     }
 
-    private function log($prefix, $msg) {
+    private function log($prefix, $msg)
+    {
         error_log($prefix . " - " . date('Y-m-d H:i:s') . " - " . $msg . "\n", 3, $this->LOG_FILE);
     }
 
-    private function saveToken($token) {
+    private function saveToken($token)
+    {
         $myfile = fopen($this->TOKEN_FILE, "w");
         fwrite($myfile, $token);
         fclose($myfile);
     }
 
 
-    private function getToken() {
-        if(file_exists($this->COOKIE_JAR)) {
+    private function getToken()
+    {
+        if (file_exists($this->TOKEN_FILE)) {
             $myfile = fopen($this->TOKEN_FILE, "r");
             $token = fgets($myfile);
             fclose($myfile);
             return $token;
-        }
-        else
-        {
+        } else {
             return "";
         }
     }
 
-    private function isOK($curl, $curl_response) {
-        $this->logInfo("HTTP Response: " . $curl_response);
-        
-        if($curl_response === false) {
+    private function isOK($curl, $curl_response)
+    {
+        if ($curl_response === false) {
             return false;
         }
 
         $info = curl_getinfo($curl);
         $this->logInfo("HTTP CODE: " . $info['http_code']);
-        if($info['http_code'] !== 200) {
-            return false;
-        }
-
-        $code = json_decode($curl_response)->{'code'};
-        $this->logInfo("CODE: " .$code);
-        if( !empty($code) && $code !== 200) {
+        if ($info['http_code'] !== 200) {
             return false;
         }
 
         return true;
     }
-
-
 }
-
-/*$url = "https://www.fshare.vn/file/BA7TDZNZQHUL";
-$username = "zang_itu@yahoo.com";
-$password = "asd123";
-
-$client = new SynoFileHostingFshareVN($url, $username, $password, NULL);
-$client->GetDownloadInfo();
-echo "DONE";*/
-
-?>
